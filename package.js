6a3f0a0757b627cec7051e7be01e8b9c8ddcb48a
Alex Scott <codetheorist@outlook.com>
2024-09-09 12:15:10 +0100

    chore: finish sorting project config

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 /* eslint-env meteor */
 
 Package.describe({
-  name: 'gotrash:theme', // https://atmospherejs.com/twbs/bootstrap
+  name: 'gotrash:theme', // https://atmospherejs.com/gotrash/theme
   summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
   version: '5.3.3',
   git: 'https://github.com/gotrash/theme.git'
@@ -12,7 +12,7 @@ Package.describe({
 Package.onUse(api => {
   api.versionsFrom('METEOR@1.0')
   api.addFiles([
-    'dist/css/bootstrap.css',
-    'dist/js/bootstrap.js'
+    'dist/css/gotrash.css',
+    'dist/js/gotrash.js'
   ], 'client')
 })
